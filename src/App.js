3b3c9f57ff8d6db0d8b10778e1c9c13a84c51671
tejastamkar <72966382+tejastamkar@users.noreply.github.com>
2021-10-17 19:01:56 +0530

    added the accent with bg

--- src/App.js
+++ src/App.js
@@ -6,6 +6,29 @@ import NavBar from "./Nav";
 function App() {
   const mtheme = Switcher(),
     Setmtheme = mtheme.sets;
+  switch (mtheme.colorTheme) {
+    case "black":
+      document.getElementById("main").style.background = "black";
+      break;
+    case "blue":
+      document.getElementById("main").style.background = "#e8faff";
+      break;
+    case "pink":
+      document.getElementById("main").style.background = "#ffe8fa";
+      break;
+    case "red":
+      document.getElementById("main").style.background = "#ffe8e8";
+      break;
+    case "yellow":
+      document.getElementById("main").style.background = "#feffe8";
+      break;
+    case "lavendar":
+      document.getElementById("main").style.background = "#ffe8fe";
+      break;
+    case "green":
+      document.getElementById("main").style.background = "#e8ffec";
+      break;
+  }
   return (
     <div className={`style ${mtheme.colorTheme}`} id="bg">
       <>
